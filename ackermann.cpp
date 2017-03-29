2fbf54015cced22ff7a066ef19e52edb2d4ef8d5
Gerry Candy <candy@smbcnikko-cm.com>
2017-03-29 08:58:15 +0100

    sod

--- ackermann.cpp
+++ ackermann.cpp
@@ -485,6 +485,11 @@ int ackermann(int x, int y){
         auto root{ fac.call("A", args)};
 
         eval_context ctx;
+        ctx
+                .push(transforms::constant_folding())
+                .push(ackermann_function())
+                .push(transforms::constant_folding())
+                .push(transforms::plus_folding());
         eval(ctx, root);
 
         if( root->get_kind() == expr::kind_constant ){
@@ -522,7 +527,18 @@ TEST( algebra_ackermann, 4_x){
 #endif
 
 
-int main(){
+#include <cassert>
+void other_test(){
+        #define _(x,y,r) do{ auto ret{ackermann(x,y)}; std::cout << "A(" << x << ", " << y << ") " << ( r == ret ? "OK" : "FAILED" ) << "\n"; }while(0);
+        /* m\n        0          1          2         3          4 */
+        /*  0  */ _(0,0,1)  _(0,1,2)   _(0,2,3)    _(0,3,4)    _(0,4,5)
+        /*  1  */ _(1,0,2)  _(1,1,3)   _(1,2,4)    _(1,3,5)    _(1,4,6)
+        /*  2  */ _(2,0,3)  _(2,1,5)   _(2,2,7)    _(2,3,9)    _(2,4,11)
+        /*  3  */ _(3,0,5)  _(3,1,13)  _(3,2,29)   _(3,3,61)   _(3,4,125)  _(3,5,253)   _(3,6,509) _(3,7,1021)
+        #undef _
+}
+
+void github_render(){
         factory fac;
         auto root{ fac.call("A", fac.constant(3), fac.constant(3)) };
 
@@ -541,8 +557,10 @@ int main(){
                         break;
                 std::cout << *root << "\n";
         }
+}
 
-
+int main(){
+        other_test();
 }
          
 // vim:tw=8 sw=8
